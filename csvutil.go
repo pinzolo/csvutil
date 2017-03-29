430a6e2077bcb4c149908583c3ba8b3a0a15d4c7
pinzolo <pinzolo@gmail.com>
2017-03-29 23:36:32 +0900

    First implementation.

---
+++ csvutil.go
@@ -0,0 +1,64 @@
+package csvutil
+
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+)
+
+// UTF8BOM is bytes for byte order mark of UTF8.
+var UTF8BOM = [3]byte{0xEF, 0xBB, 0xBF}
+
+// NewCsvReader returns *csv.Reader for UTF8.
+// If source has BOM, returns true as second return value.
+func NewCsvReader(r io.Reader) (*csv.Reader, bool) {
+	var (
+		br  *bufio.Reader
+		ok  bool
+		bom bool
+	)
+	if br, ok = r.(*bufio.Reader); !ok {
+		br = bufio.NewReader(r)
+	}
+
+	bs, err := br.Peek(len(UTF8BOM))
+	if err != nil {
+		return csv.NewReader(br), false
+	}
+	if bs[0] == UTF8BOM[0] && bs[1] == UTF8BOM[1] && bs[2] == UTF8BOM[2] {
+		br.Discard(len(UTF8BOM))
+		bom = true
+	}
+
+	return csv.NewReader(br), bom
+}
+
+// NewCsvWriter returns *csv.Writer for UTF8.
+// If true as bom, Writer writes BOM at the top.
+func NewCsvWriter(w io.Writer, bom bool) *csv.Writer {
+	var (
+		bw *bufio.Writer
+		ok bool
+	)
+	if bw, ok = w.(*bufio.Writer); !ok {
+		bw = bufio.NewWriter(w)
+	}
+
+	if bom {
+		bw.Write(UTF8BOM[:])
+	}
+	return csv.NewWriter(bw)
+}
+
+// NewCsvReaderWithEncoding returns *csv.Reader for given encoding.
+func NewCsvReaderWithEncoding(r io.Reader, e encoding.Encoding) *csv.Reader {
+	return csv.NewReader(transform.NewReader(r, e.NewDecoder()))
+}
+
+// NewCsvWriterWithEncoding returns *csv.Writer for given encoding.
+func NewCsvWriterWithEncoding(w io.Writer, e encoding.Encoding) *csv.Writer {
+	return csv.NewWriter(transform.NewWriter(w, e.NewEncoder()))
+}
---
+++ csvutil_test.go
@@ -0,0 +1,153 @@
+package csvutil
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+func TestNewCsvReaderWithoutBOM(t *testing.T) {
+	f, err := os.Open("testdata/utf8.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	r, bom := NewCsvReader(f)
+	if bom {
+		t.Errorf("Cannot check BOM. Expect: false, but got %v", bom)
+	}
+	record, err := r.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if record[0] != "名前" {
+		t.Errorf("Cannot read csv. Expect: 名前, but got %v", record[0])
+	}
+}
+
+func TestNewCsvReaderWithBOM(t *testing.T) {
+	f, err := os.Open("testdata/utf8_with_bom.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	r, bom := NewCsvReader(f)
+	if !bom {
+		t.Errorf("Cannot check BOM. Expect: true, but got %v", bom)
+	}
+	record, err := r.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if record[0] != "名前" {
+		t.Errorf("Cannot read csv. Expect: 名前, but got %v", record[0])
+	}
+}
+
+func TestNewCsvReaderWithShortCSV(t *testing.T) {
+	f, err := os.Open("testdata/short.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	r, bom := NewCsvReader(f)
+	if bom {
+		t.Errorf("Cannot check BOM. Expect: false, but got %v", bom)
+	}
+	record, err := r.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if record[0] != "a" {
+		t.Errorf("Cannot read csv. Expect: a, but got %v", record[0])
+	}
+}
+
+func TestNewCsvWriterWithoutBOM(t *testing.T) {
+	b := &bytes.Buffer{}
+	w := NewCsvWriter(b, false)
+	w.Write([]string{"名前", "個数"})
+	w.Flush()
+	if b.Bytes()[0] == UTF8BOM[0] {
+		t.Error("Expect: No BOM, but got BOM in the top.")
+	}
+	if !strings.Contains(b.String(), "名前,個数") {
+		t.Error("Failed writing CSV.", b.String())
+	}
+}
+
+func TestNewCsvWriterWithBOM(t *testing.T) {
+	b := &bytes.Buffer{}
+	w := NewCsvWriter(b, true)
+	w.Write([]string{"名前", "個数"})
+	w.Flush()
+	bs := b.Bytes()
+	if bs[0] != UTF8BOM[0] && bs[1] != UTF8BOM[1] && bs[2] != UTF8BOM[2] {
+		t.Error("Expect: With BOM, but colud not get BOM.")
+	}
+	if !strings.Contains(b.String(), "名前,個数") {
+		t.Error("Failed writing CSV.", b.String())
+	}
+}
+
+func TestNewCsvReaderWithShiftJIS(t *testing.T) {
+	f, err := os.Open("testdata/sjis.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	r := NewCsvReaderWithEncoding(f, japanese.ShiftJIS)
+	record, err := r.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if record[0] != "名前" {
+		t.Errorf("Cannot read csv. Expect: 名前, but got %v", record[0])
+	}
+}
+
+func TestNewCsvReaderWithEUCJP(t *testing.T) {
+	f, err := os.Open("testdata/eucjp.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	defer f.Close()
+	r := NewCsvReaderWithEncoding(f, japanese.EUCJP)
+	record, err := r.Read()
+	if err != nil {
+		t.Error(err)
+	}
+	if record[0] != "名前" {
+		t.Errorf("Cannot read csv. Expect: 名前, but got %v", record[0])
+	}
+}
+
+func TestNewCsvWriterWithEncoding(t *testing.T) {
+	for _, e := range []encoding.Encoding{japanese.ShiftJIS, japanese.EUCJP} {
+		b := &bytes.Buffer{}
+		w := NewCsvWriterWithEncoding(b, e)
+		w.Write([]string{"名前", "個数"})
+		w.Flush()
+		s, err := toUTF8(b.Bytes(), e)
+		if err != nil {
+			t.Error(err)
+		}
+		if !strings.Contains(s, "名前,個数") {
+			t.Error("Failed writing CSV.", s)
+		}
+	}
+}
+
+func toUTF8(bs []byte, e encoding.Encoding) (string, error) {
+	b, err := ioutil.ReadAll(transform.NewReader(bytes.NewReader(bs), e.NewDecoder()))
+	if err != nil {
+		return "", err
+	}
+	return string(b), err
+}
