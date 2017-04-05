1475cd64837f69a848e125da9a361f9e559ad512
pinzolo <pinzolo@gmail.com>
2017-04-05 22:45:18 +0900

    refs #8 Add `generate` subcommand.

---
+++ generate_test.go
@@ -0,0 +1,172 @@
+package csvutil
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGenerateWithoutSize(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count: 3,
+	}
+	if err := Generate(w, o); err == nil {
+		t.Error("Generate without size should raise error.")
+	}
+}
+
+func TestGenerateWithNegativeSize(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count: 3,
+		Size:  -1,
+	}
+	if err := Generate(w, o); err == nil {
+		t.Error("Generate with negative size should raise error.")
+	}
+}
+
+func TestGenerateWithoutCount(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Size: 3,
+	}
+	if err := Generate(w, o); err == nil {
+		t.Error("Generate without size should raise error.")
+	}
+}
+
+func TestGenerateWithNegativeCount(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count: -1,
+		Size:  3,
+	}
+	if err := Generate(w, o); err == nil {
+		t.Error("Generate with negative size should raise error.")
+	}
+}
+
+func TestGenerate(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count: 3,
+		Size:  3,
+	}
+	expected := `column1,column2,column3
+,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
+
+func TestGenerateWithHeaders(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count:   3,
+		Size:    3,
+		Headers: []string{"foo", "bar", "baz"},
+	}
+	expected := `foo,bar,baz
+,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
+
+func TestGenerateWithNoHeader(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count:    3,
+		Size:     3,
+		NoHeader: true,
+	}
+	expected := `,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
+
+func TestGenerateWithGreaterSizeThanHeadersLength(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count:   3,
+		Size:    3,
+		Headers: []string{"foo", "bar"},
+	}
+	expected := `foo,bar,column1
+,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
+
+func TestGenerateWithLessSizeThanHeadersLength(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count:   3,
+		Size:    3,
+		Headers: []string{"foo", "bar", "baz", "xxx"},
+	}
+	expected := `foo,bar,baz
+,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
+
+func TestGenerateWithNoHeaderButGivenHeaders(t *testing.T) {
+	w := &bytes.Buffer{}
+	o := GenerateOption{
+		Count:    3,
+		Size:     3,
+		NoHeader: true,
+		Headers:  []string{"foo", "bar", "baz"},
+	}
+	expected := `,,
+,,
+,,
+`
+	if err := Generate(w, o); err != nil {
+		t.Error(err)
+	}
+
+	if actual := w.String(); actual != expected {
+		t.Errorf("Expectd: %s, but got %s", expected, actual)
+	}
+}
